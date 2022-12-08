3943b421c287e04e5c2cac1aafe98f1326bef4df
Nilton César <cesar.n.2@hotmail.com>
2022-12-08 20:47:44 -0300

    filter

--- filter.js
+++ filter.js
@@ -5,6 +5,7 @@
 
 const numeros = [0,1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90]
 const numeros2 = numeros.filter(valor => valor >= 10)
+
 console.log(numeros2)
 
 //Retorne as pessoas que tem o nome com 7 letras ou mais
