1aa2aba15407a33d3dd95f789fb3753e4d774a3f
Nilton César <cesar.n.2@hotmail.com>
2022-12-07 22:37:01 -0300

    Map

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ const numeros = [10,20,30,40,50,60,70]
 
 const numerosEmDobro = numeros.map(valor => valor * 343)
 
-console.log(numerosEmDobro)
+//console.log(numerosEmDobro)
 
 //Para cada elemento: 
 //Retorne apenas uma string com o nome da pessoas
@@ -20,3 +20,8 @@ const pessoas = [
   {nome:'Rosana', idade: 32},
   {nome:'Wallace', idade: 47}
 ]
+
+const nomes = pessoas.map(obj => obj.nome)
+const idades = pessoas.map(obj => ({idade: obj.idade}))
+//console.log(nomes)
+console.log(idades)
